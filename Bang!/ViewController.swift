e39a9922ba5bb66f6ff6c40ea241322877354c66
Charlize Dang <charlize09@hotmail.com>
2019-10-26 10:58:42 -0700

    Initial Commit

---
+++ Bang!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bang!
+//
+//  Created by Charlize Dang on 2019-10-26.
+//  Copyright © 2019 Charlize Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
